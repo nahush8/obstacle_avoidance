d9dd14a45637ef3a0fa009b34917b0971e1b5a9f
nahushg <nahushg@vt.edu>
2017-05-17 18:18:29 -0400

    GPQ version 1

--- gpq.py
+++ gpq.py
@@ -15,6 +15,11 @@ import gameEngine
 record = []
 
 class gp_prediction():
+	def __init__(self):
+
+		self.kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-1, 1e1)) #C is a constant kernel and RBF is the squared exp kernel.
+		
+		self.gp = GaussianProcessRegressor(kernel=self.kernel,optimizer='fmin_l_bfgs_b' ,n_restarts_optimizer=9,alpha=1e-2)
 
 	def findMax(self,gp,next_state):
 		tempMu = 0
@@ -23,7 +28,7 @@ class gp_prediction():
 			test = next_state + [x]
 			arrayList.append(test)
 		arrayList = np.array(arrayList)
-		tempMu,sigma = gp.predict(arrayList, return_std=True, return_cov=False) 
+		tempMu,sigma = self.gp.predict(arrayList, return_std=True, return_cov=False) 
 
 		return max(tempMu)
 
@@ -31,32 +36,53 @@ class gp_prediction():
 		inputX = []
 		outputY = []
 
-		kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-1, 1e1)) #C is a constant kernel and RBF is the squared exp kernel.
 		
-		gp = GaussianProcessRegressor(kernel=kernel,optimizer='fmin_l_bfgs_b' ,n_restarts_optimizer=9,alpha=1e-2)
 		for elements in record:
 			#for element in range (0,len(record)):
 			inputX.append(elements[0] + [elements[1]])
-			outputY.append((elements[2] +self.findMax(gp,elements[3])))
+			outputY.append((elements[2] +self.findMax(self.gp,elements[3])))
 			#print inputX
 		#print outputY
 		dX = np.array(inputX)
 		#print outputY
 		tX = np.array(outputY)
 		#st = time.time()
-		gp.fit(dX,tX)
+		self.gp.fit(dX,tX)
 
+	def choose_action(self,next_state):
+		tempMu = 0
+		arrayList = []
+		listMu = []
+		action_value = 0
+		for x in (0,2):
+			test = next_state + [x]
+			arrayList.append(test)
+		arrayList = np.array(arrayList)
+		tempMu,sigma = self.gp.predict(arrayList, return_std=True, return_cov=False) 
+		listMu = list(tempMu)
+		maxIndex  = listMu.index(max(listMu))
+		tempList = arrayList[maxIndex]
+		#print tempList
+		action_value = tempList[6]
+		#print action_value 
+		return action_value
 
 if __name__ == "__main__":
+	i = 0
 	game_obj = gameEngine.GameState()
 	gp_obj = gp_prediction()
 	prev_state = [[20,20,20,20,20,20]]
+	next_state = [[20,20,20,20,20,20]]
 	while True:
-		action = random.randint(0, 2)
+		if i == 0:
+			action = random.randint(0, 2)
+		else:
+			action = gp_obj.choose_action(next_state.tolist()[0])
 		curr_reward, next_state = game_obj.frame_step(action)
 		time.sleep(0.1)
 		prev_state = next_state
 		record.append([prev_state.tolist()[0],action,curr_reward,next_state.tolist()[0]])
-		print len(record)
+		#print len(record)
 		if curr_reward == -500:
 			gp_obj.gpq(record)
+		i+= 1
